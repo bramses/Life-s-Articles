1e1037210585923a0bc65878c6b3a15a7687be81
Bram Adams <3282661+bramses@users.noreply.github.com>
2026-02-06 13:43:29 -0500

    Initial Commit

---
+++ Life's Articles/Life_s_ArticlesApp.swift
@@ -0,0 +1,32 @@
+//
+//  Life_s_ArticlesApp.swift
+//  Life's Articles
+//
+//  Created by Bram Adams on 2/6/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Life_s_ArticlesApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Life's ArticlesTests/Life_s_ArticlesTests.swift
@@ -0,0 +1,17 @@
+//
+//  Life_s_ArticlesTests.swift
+//  Life's ArticlesTests
+//
+//  Created by Bram Adams on 2/6/26.
+//
+
+import Testing
+@testable import Life_s_Articles
+
+struct Life_s_ArticlesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
